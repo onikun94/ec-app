48d1997406a680cce6d0e786fd2be94e0efb20f2
Taiyo Yamaguchi <onikun94@gmail.com>
2020-12-28 22:35:50 +0900

    add routing

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Provider} from 'react-dom';
+import {Provider} from 'react-dom';''
 import createStore from './reducks/store/store'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-export const store = createStore();
+import { ConnectedRouter, connectRouter } from 'connected-react-router';
+import * as History from 'history';
+
+const  history = History.createBrowserHistory();
+export const store = createStore(history);
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <ConnectedRouter history = {history}>
+      <App />
+    </ConnectedRouter>
   </Provider>,
   document.getElementById('root')
 );
---
+++ src/templates/Login.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Router from "./Router";
+import {useDispatch} from "react-redux";
+import {push} from "connected-react-router";
+
+const Login = () =>{
+    const dispatch = useDispatch();
+    return(
+        <div>
+            <h2>ログイン</h2>
+            <button onClick = {() => dispatch(push('/'))}>
+                ログイン
+            </button>
+        </div>
+    );
+};
+
+export default Login;
