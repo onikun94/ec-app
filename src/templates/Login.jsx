6bea4f45cdceba8c519848941acbe3d3c34a5743
Taiyo Yamaguchi <onikun94@gmail.com>
2020-12-30 23:00:44 +0900

    manage by re-ducks-pattern

--- src/templates/Login.jsx
+++ src/templates/Login.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
-import Router from "./Router";
 import {useDispatch} from "react-redux";
 import {push} from "connected-react-router";
+import { signInAction } from '../reducks/users/actions';
 
 const Login = () =>{
     const dispatch = useDispatch();
     return(
         <div>
             <h2>ログイン</h2>
-            <button onClick = {() => dispatch(push('/'))}>
+            <button onClick = {() => {
+                dispatch(signInAction({uid:"001",username: "oni"}))
+                dispatch(push('/'))
+            }
+            }>
                 ログイン
             </button>
         </div>
