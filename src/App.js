4e439134a4dca7bc9abdbc242f9bc067ce9be01e
Taiyo Yamaguchi <onikun94@gmail.com>
2021-01-03 23:05:23 +0900

    Add dispatch and selector

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import {useDispatch, useSelector} from "react-redux";
+import { signInAction } from './reducks/users/actions';
 
 function App() {
+  const dispatch = useDispatch();
+  const selector = useSelector((state) => state)
   return (
+
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <button onClick = {() => dispatch(signInAction({uid: "29", username: "niku"}))}>
+
+        </button>
         
       </header>
     </div>
